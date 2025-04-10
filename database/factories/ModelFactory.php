8b45218af699f5b7c56494a17daaee6b593b726f
Momenoor <momen.noor@gmail.com>
2025-04-10 10:08:32 +0400

    First Init

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Momenoor\RelatedTableField\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use Momenoor\RelatedTableField\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
